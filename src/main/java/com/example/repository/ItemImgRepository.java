3091d5aa5323aa6a5e706a978977b972785bcd73
Hyse16 <gustmd4296@hanmail.net>
2022-12-10 16:44:45 +0900

    ItemController 추가, ItemImgRepository 메소드 추가

--- src/main/java/com/example/repository/ItemImgRepository.java
+++ src/main/java/com/example/repository/ItemImgRepository.java
@@ -3,5 +3,8 @@ package com.example.repository;
 import com.example.entity.ItemImg;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+    List<ItemImg> findByItemIdOrderByIdAsc(Long ItemId);
 }
