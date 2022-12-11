f4f0014642caf88916859042dbd8dd54e444dfa3
Hyse16 <gustmd4296@hanmail.net>
2022-12-11 17:03:12 +0900

    Revert "ItemController 추가, ItemImgRepository 메소드 추가"
    This reverts commit 3091d5aa5323aa6a5e706a978977b972785bcd73.

--- src/main/java/com/example/repository/ItemImgRepository.java
+++ src/main/java/com/example/repository/ItemImgRepository.java
@@ -3,8 +3,5 @@ package com.example.repository;
 import com.example.entity.ItemImg;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
-    List<ItemImg> findByItemIdOrderByIdAsc(Long ItemId);
 }
