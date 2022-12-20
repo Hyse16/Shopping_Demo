ee2d0d1c7c048e2560fe39c85f5fbc5d40946de5
Hyse16 <gustmd4296@hanmail.net>
2022-12-20 11:27:16 +0900

    장바구니 담기 추가

---
+++ src/test/java/com/example/controller/CartControllerTest.java
@@ -0,0 +1,73 @@
+package com.example.controller;
+
+import com.example.Service.CartService;
+import com.example.constant.ItemSellStatus;
+import com.example.dto.CartItemDto;
+import com.example.entity.CartItem;
+import com.example.entity.Item;
+import com.example.entity.Member;
+import com.example.repository.CartItemRepository;
+import com.example.repository.ItemRepository;
+import com.example.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityExistsException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class CartControllerTest {
+
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    CartService cartService;
+
+    @Autowired
+    CartItemRepository cartItemRepository;
+
+    public Item saveItem() {
+        Item item = new Item();
+        item.setItemNm("테스트 상품");
+        item.setPrice(10000);
+        item.setItemDetail("테스트 상품 상세 설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        return itemRepository.save(item);
+    }
+
+    public Member saveMember() {
+        Member member = new Member();
+        member.setEmail("test@email.com");
+        return memberRepository.save(member);
+    }
+
+    @Test
+    @DisplayName("장바구니 담기 테스트")
+    public void addcart() {
+        Item item = saveItem();
+        Member member = saveMember();
+
+        CartItemDto cartItemDto = new CartItemDto();
+        cartItemDto.setCount(5);
+        cartItemDto.setItemId(item.getId());
+
+
+        Long cartItemId = cartService.addCart(cartItemDto, member.getEmail());
+
+        CartItem cartItem = cartItemRepository.findById(cartItemId).orElseThrow(EntityExistsException::new);
+
+        assertEquals(item.getId(), cartItem.getItem().getId());
+        assertEquals(cartItemDto.getCount(), cartItem.getCount());
+    }
+}
